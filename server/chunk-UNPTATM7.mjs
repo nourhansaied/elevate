3bd4367a9d8abe6c47a8500e4cc528bf47cdea89
Nourhan Saeed <nourhansaed6@gmail.com>
2024-12-03 22:13:59 +0200

    Auto-generated commit

---
+++ server/chunk-UNPTATM7.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{Ia as o,Ja as r,Pa as i,Ra as s,V as n}from"./chunk-GBCY3QDE.mjs";import"./chunk-VVCT4QZE.mjs";var c=(()=>{class e{static{this.\u0275fac=function(t){return new(t||e)}}static{this.\u0275cmp=n({type:e,selectors:[["app-register"]],standalone:!0,features:[s],decls:2,vars:0,template:function(t,p){t&1&&(o(0,"p"),i(1,"register works!"),r())}})}}return e})();export{c as RegisterComponent};
---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-CA4D3QES.mjs";import"./chunk-ZADUOIE2.mjs";import"./chunk-ZNRTN5PJ.mjs";import"./chunk-DGTPULP3.mjs";import"./chunk-GBCY3QDE.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
