c9c2225f2e11ee3e927bbe366a7ace9e99d386e9
Nourhan Saeed <nourhansaed6@gmail.com>
2024-12-03 22:12:42 +0200

    Auto-generated commit

---
+++ browser/chunk-2Y6FFM25.js
@@ -0,0 +1,1 @@
+import{Ha as o,Ia as r,Oa as i,Qa as s,Y as n}from"./chunk-H3Y6GNUR.js";var c=(()=>{class e{static{this.\u0275fac=function(t){return new(t||e)}}static{this.\u0275cmp=n({type:e,selectors:[["app-register"]],standalone:!0,features:[s],decls:2,vars:0,template:function(t,p){t&1&&(o(0,"p"),i(1,"register works!"),r())}})}}return e})();export{c as RegisterComponent};
